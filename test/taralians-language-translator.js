6a1d361674fd0ef1b28d0a0e88b4f9c413ffb21e
kjirou <sorenariblog@gmail.com>
2018-03-23 14:27:37 +0900

    Add a few tests

---
+++ test/taralians-language-translator.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const {describe, it} = require('mocha');
+
+const {translate} = require('..');
+
+describe('taralians-language-translator', function() {
+  describe('Verification of historical facts', function() {
+    [
+      ['What is your name?', 'ムアナ ウト ホオテ ラヘ?'],
+      ['I am ナイトハルト prince of ローザリア.', 'ウ アソ ナイトハルト チミタゲ エケ ローザリア.'],
+      ['...who...are...Black! Black demon!', '...ムエ...アメ...カヤキス! カヤキス レビタ!'],
+    ].forEach(([source, translated]) => {
+      it(`"${source}" => "${translated}"`, function() {
+        assert.strictEqual(translate(source), translated);
+      })
+    });
+  });
+});
