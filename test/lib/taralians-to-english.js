945500ce6cccd7f8e78961824a7affde376e2dd7
kjirou <sorenariblog@gmail.com>
2018-04-09 20:48:46 +0900

    Escape translation if word is quoted

--- test/lib/taralians-to-english.js
+++ test/lib/taralians-to-english.js
@@ -4,11 +4,12 @@ const {describe, it} = require('mocha');
 const {translate} = require('../../lib/taralians-to-english');
 
 describe('lib/taralians-to-english', function() {
-  describe('Verification of historical facts', function() {
+  describe('Example sentences', function() {
     [
       ['ムアナ ウト ホオテ ラヘ?', 'what is your name?'],
-      //['ウ アソ ナイトハルト チミタゲ エケ ローザリア.', 'i am ナイトハルト prince of ローザリア.'],
+      ['ウ アソ "ナイトハルト" チミタゲ エケ "ローザリア".', 'i am "ナイトハルト" prince of "ローザリア".'],
       ['...ムエ...アメ...カヤキス! カヤキス レビタ!', '...who...are...black! black demon!', ],
+      ["トアニイ オト 'ニーサ'、トアニイ オト 'エロール'。", "save us 'ニーサ'、save us 'エロール'。"],
     ].forEach(([source, translated]) => {
       it(`"${source}" => "${translated}"`, function() {
         assert.strictEqual(translate(source), translated);
