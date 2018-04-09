f1d72197f75314851ef4aba551de4681e3dade7b
kjirou <sorenariblog@gmail.com>
2018-04-09 20:56:41 +0900

    As a result, proper names are reflected in E-to-T

--- test/lib/english-to-taralians.js
+++ test/lib/english-to-taralians.js
@@ -4,11 +4,13 @@ const {describe, it} = require('mocha');
 const {translate} = require('../../lib/english-to-taralians');
 
 describe('lib/english-to-taralians', function() {
-  describe('Verification of historical facts', function() {
+  describe('Example sentences', function() {
     [
       ['What is your name?', 'ムアナ ウト ホオテ ラヘ?'],
       ['I am ナイトハルト prince of ローザリア.', 'ウ アソ ナイトハルト チミタゲ エケ ローザリア.'],
+      ['I am "ナイトハルト" prince of "ローザリア".', 'ウ アソ "ナイトハルト" チミタゲ エケ "ローザリア".'],
       ['...who...are...Black! Black demon!', '...ムエ...アメ...カヤキス! カヤキス レビタ!'],
+      ["Save us 'ニーサ'、Save us 'エロール'。", "トアニイ オト 'ニーサ'、トアニイ オト 'エロール'。"],
     ].forEach(([source, translated]) => {
       it(`"${source}" => "${translated}"`, function() {
         assert.strictEqual(translate(source), translated);
